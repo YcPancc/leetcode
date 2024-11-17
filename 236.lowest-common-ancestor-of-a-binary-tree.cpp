6d8f1eec7d0819841601110c14f3221ab38fded8
YcPancc <git@ycpan.cc>
2024-11-17 09:56:56 +0800

    day21

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ * @lcpr version=30204
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lcpr-template-start
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {
+	}
+};
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+	TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+		if (!root || root == p || root == q)
+			return root;
+		TreeNode *left = lowestCommonAncestor(root->left, p, q);
+		TreeNode *right = lowestCommonAncestor(root->right, p, q);
+		if (left && right)
+			return root;
+		if (!left)
+			return right;
+		return left;
+	}
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n1\n2\n
+// @lcpr case=end
+
+ */
